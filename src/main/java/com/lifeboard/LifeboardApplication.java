347e43d5da9ac2c2a73fccad74f6c40cbef7cba8
GuilhermeRCabreira <guilhermercabreira@gmail.com>
2026-08-31 11:22:45 -0300

    chore: inicializando projeto

---
+++ src/main/java/com/lifeboard/LifeboardApplication.java
@@ -0,0 +1,13 @@
+package com.lifeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lifeboard/LifeboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifeboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
